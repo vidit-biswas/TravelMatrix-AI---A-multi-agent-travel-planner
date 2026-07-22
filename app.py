a5376fe23eb390da4257385260d0ebd10bceb72b
vidit biswas <viditbiswas18@gmail.com>
2026-07-22 14:15:00 +0530

    Added Docker support

--- app.py
+++ app.py
@@ -110,10 +110,11 @@ async def favicon():
 
 
 
+import os
+
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
     )
